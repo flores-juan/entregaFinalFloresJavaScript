2d4cdae9c6b3199bb9b3883654504bc9dbbf54e0
Juan Flores <flores97juanignacio@gmail.com>
2024-02-24 18:19:07 -0300

    reEntregaFinalFlores

---
+++ js/app.js
@@ -0,0 +1,80 @@
+
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const showAlert = document.getElementById("showAlert");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const getProducts = async () => {
+    const response = await fetch("./data.json")
+    const data = await response.json();
+
+
+    data.forEach((product) => {
+        let content = document.createElement("div");
+        content.className = "card";
+        content.innerHTML = `
+          <img src="${product.img}">
+          <h3>${product.nombre}</h3>
+          <p class="price">${product.precio} $</p>
+        `;
+
+        shopContent.append(content);
+
+        let comprar = document.createElement("button");
+        comprar.innerText = "comprar";
+        comprar.className = "comprar";
+
+        content.append(comprar);
+
+        comprar.addEventListener("click", () => {
+
+            
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+
+            if (repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        prod.cantidad++;
+                        
+                    }
+                    
+                });
+            } else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nombre: product.nombre,
+                    precio: product.precio,
+                    cantidad: product.cantidad,
+                });
+                console.log(carrito);
+                console.log(carrito.length);
+                carritoCounter();
+                saveLocal();
+            }
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Producto agregado!",
+                showConfirmButton: false,
+                timer: 1500
+            });
+          
+        });
+    });
+
+};
+
+getProducts();
+
+
+
+//set item
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+//get item
